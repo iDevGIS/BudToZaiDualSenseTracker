5cd10a7f72a1ffab75e2b4c3a596f9c320cc61da
CYB3R-R0N1N\TH668 <idevgis@gmail.com>
2025-03-25 01:44:27 +0700

    final v1

--- BudToZaiDualSenseTracker/Form1.cs
+++ BudToZaiDualSenseTracker/Form1.cs
@@ -3,17 +3,136 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
 
 namespace BudToZaiDualSenseTracker
 {
     public partial class Form1 : Form
     {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
         public Form1()
         {
             InitializeComponent();
+
+            // ตั้งค่า FormBorderStyle เป็น None
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            // ตั้งค่า BackColor เป็นสีที่คุณต้องการให้ใส
+            //this.BackColor = Color.LimeGreen;
+            //this.BackColor = Color.Black;
+
+            // ตั้งค่า TransparencyKey ให้ตรงกับสีของ BackColor
+            //this.TransparencyKey = Color.LimeGreen;
+            //this.TransparencyKey = Color.Black;
+
+            //this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            //this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+            //this.MouseUp += new MouseEventHandler(Form1_MouseUp);
+
+
+            InitWebView();
+
+
+
+
+            //PictureBox clickableArea = new PictureBox();
+            //clickableArea.BackColor = Color.FromArgb(1, 255, 255, 255); // Alpha 1 (เกือบใส แต่ยังรับ mouse ได้)
+            //clickableArea.Size = new Size(344, 197);
+            //clickableArea.Location = new Point(9, 9);
+
+            //clickableArea.Click += (s, e) => {
+            //    MessageBox.Show("Click ได้ ไม่ทะลุ!");
+            //};
+
+            //this.Controls.Add(clickableArea);
+
+
+
+            //สมมติว่าโซนที่คลิกได้เป็นสี่เหลี่ยมตรงกลาง
+            Label clickZone = new Label();
+            //clickZone.Text = "Click Here";
+            clickZone.TextAlign = ContentAlignment.MiddleCenter;
+            clickZone.Size = new Size(20, 20);
+            clickZone.Location = new Point(130, 100);
+            clickZone.BackColor = Color.FromArgb(50, Color.White);
+            clickZone.ForeColor = Color.Black;
+            clickZone.Cursor = Cursors.Hand;
+
+            // //clickZone.Click += (s, e) => {
+            // //    MessageBox.Show("You clicked inside the zone!");
+            // //};
+
+            this.Controls.Add(clickZone);
+
+
+
         }
+
+        private async void InitWebView()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.Settings.IsWebMessageEnabled = true;
+
+            string html = Properties.Resources.index;
+            string tempPath = Path.Combine(Path.GetTempPath(), "index.html");
+            File.WriteAllText(tempPath, html);
+            webView21.Source = new Uri(tempPath);
+
+            webView21.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+        }
+        private void CoreWebView2_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            string message = e.TryGetWebMessageAsString();
+            if (message == "mousedown")
+            {
+                dragging = true;
+                dragCursorPoint = Cursor.Position;
+                dragFormPoint = this.Location;
+            }
+            else if (message == "mousemove" && dragging)
+            {
+                Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(diff));
+            }
+            else if (message == "mouseup")
+            {
+                dragging = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //private void Form1_MouseDown(object sender, MouseEventArgs e)
+        //{
+        //    dragging = true;
+        //    dragCursorPoint = Cursor.Position;
+        //    dragFormPoint = this.Location;
+        //}
+
+        //private void Form1_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if (dragging)
+        //    {
+        //        Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+        //        this.Location = Point.Add(dragFormPoint, new Size(diff));
+        //    }
+        //}
+
+        //private void Form1_MouseUp(object sender, MouseEventArgs e)
+        //{
+        //    dragging = false;
+        //}
+
     }
 }
--- BudToZaiDualSenseTracker/Properties/Resources.Designer.cs
+++ BudToZaiDualSenseTracker/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BudToZaiDualSenseTracker.Properties
-{
-
-
+namespace BudToZaiDualSenseTracker.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,79 @@ namespace BudToZaiDualSenseTracker.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BudToZaiDualSenseTracker.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html lang=&quot;en&quot;&gt;
+        ///
+        ///&lt;head&gt;
+        ///  &lt;meta charset=&quot;UTF-8&quot; /&gt;
+        ///  &lt;title&gt;BudToZai-PS5&lt;/title&gt;
+        ///  &lt;style&gt;
+        ///    html,
+        ///    body {
+        ///      margin: 0;
+        ///      padding: 0;
+        ///      height: 100%;
+        ///      width: 100%;
+        ///      background: #11111100;
+        ///      font-family: sans-serif;
+        ///      box-sizing: border-box;
+        ///    }
+        ///
+        ///    #wrapper {
+        ///      display: flex;
+        ///      align-items: center;
+        ///      justify-content: center;
+        ///      padding: 30px;
+        ///      height: 100vh;
+        ///      width: 100vw;
+        ///      box-sizing: border-box;
+        /// [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string index {
+            get {
+                return ResourceManager.GetString("index", resourceCulture);
             }
         }
     }
