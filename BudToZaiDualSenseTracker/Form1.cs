972e33fc328847e418f952e556f3e5f1701ff8e6
CYB3R-R0N1N\TH668 <idevgis@gmail.com>
2025-03-26 10:15:36 +0700

    autosave HUD location

--- BudToZaiDualSenseTracker/Form1.cs
+++ BudToZaiDualSenseTracker/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using Microsoft.Web.WebView2.Core;
 
@@ -17,62 +12,27 @@ namespace BudToZaiDualSenseTracker
         private bool dragging = false;
         private Point dragCursorPoint;
         private Point dragFormPoint;
+
         public Form1()
         {
             InitializeComponent();
-
-            // ตั้งค่า FormBorderStyle เป็น None
             this.FormBorderStyle = FormBorderStyle.None;
-
-            // ตั้งค่า BackColor เป็นสีที่คุณต้องการให้ใส
-            //this.BackColor = Color.LimeGreen;
-            //this.BackColor = Color.Black;
-
-            // ตั้งค่า TransparencyKey ให้ตรงกับสีของ BackColor
-            //this.TransparencyKey = Color.LimeGreen;
-            //this.TransparencyKey = Color.Black;
-
-            //this.MouseDown += new MouseEventHandler(Form1_MouseDown);
-            //this.MouseMove += new MouseEventHandler(Form1_MouseMove);
-            //this.MouseUp += new MouseEventHandler(Form1_MouseUp);
-
+            if (Properties.Settings.Default.FormLocation != Point.Empty)
+            {
+                this.StartPosition = FormStartPosition.Manual;
+                this.Location = Properties.Settings.Default.FormLocation;
+            }
 
             InitWebView();
 
-
-
-
-            //PictureBox clickableArea = new PictureBox();
-            //clickableArea.BackColor = Color.FromArgb(1, 255, 255, 255); // Alpha 1 (เกือบใส แต่ยังรับ mouse ได้)
-            //clickableArea.Size = new Size(344, 197);
-            //clickableArea.Location = new Point(9, 9);
-
-            //clickableArea.Click += (s, e) => {
-            //    MessageBox.Show("Click ได้ ไม่ทะลุ!");
-            //};
-
-            //this.Controls.Add(clickableArea);
-
-
-
-            //สมมติว่าโซนที่คลิกได้เป็นสี่เหลี่ยมตรงกลาง
             Label clickZone = new Label();
-            //clickZone.Text = "Click Here";
             clickZone.TextAlign = ContentAlignment.MiddleCenter;
             clickZone.Size = new Size(20, 20);
             clickZone.Location = new Point(130, 100);
             clickZone.BackColor = Color.FromArgb(50, Color.White);
             clickZone.ForeColor = Color.Black;
             clickZone.Cursor = Cursors.Hand;
-
-            // //clickZone.Click += (s, e) => {
-            // //    MessageBox.Show("You clicked inside the zone!");
-            // //};
-
             this.Controls.Add(clickZone);
-
-
-
         }
 
         private async void InitWebView()
@@ -87,6 +47,7 @@ namespace BudToZaiDualSenseTracker
 
             webView21.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
         }
+
         private void CoreWebView2_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
         {
             string message = e.TryGetWebMessageAsString();
@@ -112,27 +73,11 @@ namespace BudToZaiDualSenseTracker
 
         }
 
-
-        //private void Form1_MouseDown(object sender, MouseEventArgs e)
-        //{
-        //    dragging = true;
-        //    dragCursorPoint = Cursor.Position;
-        //    dragFormPoint = this.Location;
-        //}
-
-        //private void Form1_MouseMove(object sender, MouseEventArgs e)
-        //{
-        //    if (dragging)
-        //    {
-        //        Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
-        //        this.Location = Point.Add(dragFormPoint, new Size(diff));
-        //    }
-        //}
-
-        //private void Form1_MouseUp(object sender, MouseEventArgs e)
-        //{
-        //    dragging = false;
-        //}
-
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            Properties.Settings.Default.FormLocation = this.Location;
+            Properties.Settings.Default.Save();
+        }
     }
 }
